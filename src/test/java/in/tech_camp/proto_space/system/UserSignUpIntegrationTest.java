df59524bd2bada301ba996846cb34ec83715c794
shots0824 <shota.shimizu@smart-group.co.jp>
2026-07-02 09:29:13 +0900

    レビューの修正

--- src/test/java/in/tech_camp/proto_space/system/UserSignUpIntegrationTest.java
+++ src/test/java/in/tech_camp/proto_space/system/UserSignUpIntegrationTest.java
@@ -5,15 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.transaction.annotation.Transactional;
 
 import in.tech_camp.proto_space.entity.User;
 import in.tech_camp.proto_space.repositories.UserRepository;
 
-@SpringBootTest
-@ActiveProfiles("test")
+
 @Transactional
 
 public class UserSignUpIntegrationTest {
@@ -28,15 +26,22 @@ public class UserSignUpIntegrationTest {
       User user1 = new User();
       user1.setEmail("test@test.com");
       user1.setPassword("123456");
+      user1.setName("山田太郎");
+      user1.setProfile("プロフィール");
+      user1.setAffiliation("所属");
+      user1.setPosition("職種");
       userRepository.save(user1);
 
         // ② 同じメールで2件目を作る
       User user2 = new User();
       user2.setEmail("test@test.com");
       user2.setPassword("123456");
-
+      user2.setName("山田太郎");
+      user2.setProfile("プロフィール");
+      user2.setAffiliation("所属");
+      user2.setPosition("職種");
       // ③ 保存すると例外が発生することを確認
-      assertThrows(Exception.class, () -> {
+      assertThrows( DataIntegrityViolationException.class, () -> {
           userRepository.save(user2);
       });
   }
