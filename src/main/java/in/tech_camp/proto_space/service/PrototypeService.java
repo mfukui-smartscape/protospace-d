e0d55e29126490d24f687837a922d91283daa151
Yusei3160 <yusei.ishimoto@smart-group.co.jp>
2026-07-03 16:10:58 +0900

    feat: PrototypeServiceを追加

---
+++ src/main/java/in/tech_camp/proto_space/service/PrototypeService.java
@@ -0,0 +1,24 @@
+package in.tech_camp.proto_space.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import in.tech_camp.proto_space.entity.Prototype;
+import in.tech_camp.proto_space.repository.PrototypeRepository;
+
+@Service
+public class PrototypeService {
+
+    private final PrototypeRepository prototypeRepository;
+
+    public PrototypeService(
+            PrototypeRepository prototypeRepository) {
+
+        this.prototypeRepository = prototypeRepository;
+    }
+
+    public List<Prototype> findAll() {
+        return prototypeRepository.findAll();
+    }
+}
