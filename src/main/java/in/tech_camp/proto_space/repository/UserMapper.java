f6d805b953bef783b64455df5de12b8f6993a01d
shots0824 <shota.shimizu@smart-group.co.jp>
2026-07-06 16:28:40 +0900

    Update UserMapper.java

--- src/main/java/in/tech_camp/proto_space/repository/UserMapper.java
+++ src/main/java/in/tech_camp/proto_space/repository/UserMapper.java
@@ -1,3 +1,5 @@
+package in.tech_camp.proto_space.repository;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
